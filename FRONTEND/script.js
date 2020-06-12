4e97326846c737723b1f1e915f523b286d48d419
Caio Silva <caio_silvabatista@hotmail.com>
2020-06-12 16:33:54 -0300

    Boas práticas aplicada e nova função.
    Nova função showComment.

--- FRONTEND/script.js
+++ FRONTEND/script.js
@@ -1,3 +1,4 @@
+
 function getPost() {
   fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json())
@@ -13,9 +14,7 @@ function getComment(postId) {
   fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
     .then(response => response.json())
     .then(json => {
-      document.getElementById(`post-${postId}`).innerHTML += `<h6>Comentários</h6><div id="comment-${postId}">`;
-      // json = `${json}`;
-      console.log(json[1].id);
+      document.getElementById(`post-${postId}`).innerHTML += `<h6 class="comment" onclick="showComment(${postId})" >Comentários<small><br>Cique aqui para ler os comentários.</small></h6><div id="comment-${postId}" hidden>`;
       json.forEach(comment => { commentHtml(postId, comment.name, comment.email, comment.body); })
     })
     .catch(function () {
@@ -24,14 +23,13 @@ function getComment(postId) {
 
 function postHtml(postId, postTitle, postBody) {
   // "StringBuilder" para gerar o HTML de determinada postagem.
-  var postHtml = [];
-  postHtml.push(
+  var postHtml = [
     '<div class="card">',
     `<div id="post-${postId}" class="post">`,
     `<h2>${postTitle}</h2>`,
     `<p>${postBody}</p>`,
     '<hr>'
-  );
+  ];
 
   // Adiciona o POST atual na página HTML
   document.getElementById('main').innerHTML += postHtml.join("");
@@ -42,16 +40,23 @@ function postHtml(postId, postTitle, postBody) {
 
 function commentHtml(postId, commentName, commentEmail, commentBody) {
   // "StringBuilder" para gerar o HTML de determinado comentário.
-  console.log('bateu aq');
-  var commentHtml = [];
-  commentHtml.push(
+  var commentHtml = [
     '<div class="post-comment">',
     `<h4>Nome: <small>${commentName}</small> </h4>`,
     `<h4>E-mail: <small>${commentEmail}</small></h4>`,
     `<p>${commentBody}</p>`,
     '</div><hr class="post-comment">'
-  );
+  ];
 
   // Adiciona o COMENTÁRIO atual na página HTML
   document.getElementById(`comment-${postId}`).innerHTML += commentHtml.join("");
+}
+
+function showComment(postId) {
+  if (document.getElementById(`comment-${postId}`).hasAttribute("hidden")){
+    document.getElementById(`comment-${postId}`).removeAttribute("hidden");
+  }
+  else{
+    document.getElementById(`comment-${postId}`).setAttribute("hidden", "hidden");
+  }
 }
