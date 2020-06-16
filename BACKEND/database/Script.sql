aef4fcaccbb4af95b2d3e178a291ecc425f60a6b
Caio Silva <caio_silvabatista@hotmail.com>
2020-06-16 11:01:00 -0300

    Script de criação do banco de dados.

---
+++ BACKEND/database/Script.sql
@@ -0,0 +1,31 @@
+-- NAVE.RS API DATABASE
+-- DROP TABLE `POST`
+-- DROP TABLE `COMMENT`
+
+-- USA O BANCO DE DADOS
+USE sql10348720;
+
+-- CRIA A TABELA POST
+SELECT * FROM `posts`;
+CREATE TABLE IF NOT EXISTS `posts` (`
+id` INTEGER NOT NULL auto_increment , 
+`title` VARCHAR(255),
+ `body` VARCHAR(255),
+ `createdAt` DATETIME NOT NULL,
+ `updatedAt` DATETIME NOT NULL,
+ PRIMARY KEY (`id`)
+ );
+ 
+-- CRIA A TABELA COMMENT
+SELECT * FROM `comments`;
+CREATE TABLE IF NOT EXISTS `comments` (
+`id` INTEGER , 
+`message` VARCHAR(255), 
+`post_id` INTEGER,
+ `createdAt` DATETIME NOT NULL, 
+ `updatedAt` DATETIME NOT NULL, 
+ PRIMARY KEY (`id`)
+ );
+
+-- ADICIONA AS CONSTRAINTS NA TABELA COMMENT
+ALTER TABLE `comments` ADD CONSTRAINT `fk_post_id` FOREIGN KEY (post_id) REFERENCES `posts` (id);
